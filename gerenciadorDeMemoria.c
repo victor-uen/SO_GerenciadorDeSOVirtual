88f727989c0a2804154b736f6e01ca2b32b45129
Victor Uen <vtfu@cesar.school>
2025-11-01 14:02:51 -0300

    Implementação do disco e exibição da memória

--- gerenciadorDeMemoria.c
+++ gerenciadorDeMemoria.c
@@ -3,22 +3,31 @@
 
 #define TAM_MEMORIA 3
 #define TAM_REFERENCIAS 12
+#define TAM_DISCO 10
 
 typedef struct {
     int paginas[TAM_MEMORIA];
     int contador;
 } Memoria;
 
+int disco[TAM_DISCO];
+
 void inicializar_memoria(Memoria *memoria) {
-    for (int i = 0; i < TAM_MEMORIA; i++) {
+    for (int i = 0; i < TAM_MEMORIA; i++)
         memoria->paginas[i] = -1;
-    }
     memoria->contador = 0;
+
+    for (int i = 0; i < TAM_DISCO; i++)
+        disco[i] = i;
 }
 
-int main() {
-    Memoria memoria;
-    inicializar_memoria(&memoria);
-    printf("Memória inicializada.\n");
-    return 0;
+void exibir_memoria(Memoria *memoria) {
+    printf("Memória: ");
+    for (int i = 0; i < TAM_MEMORIA; i++) {
+        if (memoria->paginas[i] == -1)
+            printf("[ ] ");
+        else
+            printf("[%d] ", memoria->paginas[i]);
+    }
+    printf("\n");
 }
